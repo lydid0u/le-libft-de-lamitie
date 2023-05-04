940df55e14ee5d5e69b937d6580087c84e2b6c85
lydid0u <122122182+lydid0u@users.noreply.github.com>
2023-05-04 14:32:26 +0200

    okokok

---
+++ ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lboudjel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/03 19:49:53 by lboudjel          #+#    #+#             */
+/*   Updated: 2023/05/03 19:49:53 by lboudjel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/libft.h"
+
+void	*ft_memmove(void *destina, const void *source, size_t n)
+{
+	unsigned char		*dest;
+	const unsigned char	*src;
+	size_t				i;
+
+	i = 0;
+	dest = (unsigned char *) destina;
+	src = (const unsigned char *) source;
+	if (src < dest)
+	{
+		while (n--)
+		{
+			dest[n] = src[n];
+		}
+	}
+	else
+	{
+		while (n > i)
+		{
+			dest[i] = src[i];
+			i++;
+		}
+	}
+	return (dest);
+}
